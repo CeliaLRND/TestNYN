8951e1544b2fe56a74e2b231ea21f1eb70d0c33f
Celialrnd <celia.lerandy@gmail.com>
2017-02-02 17:30:07 +0800

    v2 of the website using less

--- js/script.js
+++ js/script.js
@@ -19,4 +19,45 @@ $('#gallery-icon-1,#gallery-link-1').on('click', function() {
     })
  
 });
+
+$('#gallery-icon-2,#gallery-link-2').on('click', function() {
+ 
+    $(this).lightGallery({
+        dynamic: true,
+        download: false,
+        counter: false,
+        dynamicEl: [{
+            'src': './img/proj02-3.jpg',
+            
+            }, {
+            'src': './img/proj02-4.jpg',
+            
+            }, {
+            'src': './img/proj02-5.jpg',
+            
+            }]
+    })
+ 
+});
+
+$('#gallery-icon-3,#gallery-link-3').on('click', function() {
+ 
+    $(this).lightGallery({
+        dynamic: true,
+        download: false,
+        counter: false,
+        dynamicEl: [{
+            'src': './img/proj02-3.jpg',
+            
+            }, {
+            'src': './img/proj02-4.jpg',
+            
+            }, {
+            'src': './img/proj02-5.jpg',
+            
+            }]
+    })
+ 
+});
+
 });
