85a6acfd9fb2339e31f49e927d6f727597ffd308
Celialrnd <celia.lerandy@gmail.com>
2017-02-05 11:59:36 +0800

    maj pics

--- js/script.js
+++ js/script.js
@@ -7,13 +7,34 @@ $('#gallery-icon-1,#gallery-link-1').on('click', function() {
         download: false,
         counter: false,
         dynamicEl: [{
-            'src': './img/proj02-3.jpg',
+            'src': './img/proj1/1.jpg',
             
             }, {
-            'src': './img/proj02-4.jpg',
+            'src': './img/proj1/2.jpg',
             
             }, {
-            'src': './img/proj02-5.jpg',
+            'src': './img/proj1/3.jpg',
+            
+            }, {
+            'src': './img/proj1/4.jpg',
+            
+            }, {
+            'src': './img/proj1/5.jpg',
+            
+            }, {
+            'src': './img/proj1/6.jpg',
+            
+            }, {
+            'src': './img/proj1/7.jpg',
+            
+            }, {
+            'src': './img/proj1/8.jpg',
+            
+            }, {
+            'src': './img/proj1/9.jpg',
+            
+            }, {
+            'src': './img/proj1/10.jpg',
             
             }]
     })
@@ -27,13 +48,31 @@ $('#gallery-icon-2,#gallery-link-2').on('click', function() {
         download: false,
         counter: false,
         dynamicEl: [{
-            'src': './img/proj02-3.jpg',
+            'src': './img/proj2/1.jpg',
             
             }, {
-            'src': './img/proj02-4.jpg',
+            'src': './img/proj2/2.jpg',
             
             }, {
-            'src': './img/proj02-5.jpg',
+            'src': './img/proj2/3.jpg',
+            
+            }, {
+            'src': './img/proj2/4.jpg',
+            
+            }, {
+            'src': './img/proj2/5.jpg',
+            
+            }, {
+            'src': './img/proj2/6.jpg',
+            
+            }, {
+            'src': './img/proj2/7.jpg',
+            
+            }, {
+            'src': './img/proj2/8.jpg',
+            
+            }, {
+            'src': './img/proj2/9.jpg',
             
             }]
     })
@@ -47,17 +86,237 @@ $('#gallery-icon-3,#gallery-link-3').on('click', function() {
         download: false,
         counter: false,
         dynamicEl: [{
-            'src': './img/proj02-3.jpg',
+            'src': './img/proj3/1.jpg',
             
             }, {
-            'src': './img/proj02-4.jpg',
+            'src': './img/proj3/2.jpg',
             
             }, {
-            'src': './img/proj02-5.jpg',
+            'src': './img/proj3/3.jpg',
+            
+            }, {
+            'src': './img/proj3/4.jpg',
+            
+            }, {
+            'src': './img/proj3/5.jpg',
+            
+            }, {
+            'src': './img/proj3/6.jpg',
+            
+            }, {
+            'src': './img/proj3/7.jpg',
+            
+            }, {
+            'src': './img/proj3/8.jpg',
             
             }]
     })
  
 });
 
+
+
+$('#gallery-icon-4,#gallery-link-4').on('click', function() {
+ 
+    $(this).lightGallery({
+        dynamic: true,
+        download: false,
+        counter: false,
+        dynamicEl: [{
+            'src': './img/proj4/1.jpg',
+            
+            }, {
+            'src': './img/proj4/2.jpg',
+            
+            }, {
+            'src': './img/proj4/3.jpg',
+            
+            }, {
+            'src': './img/proj4/4.jpg',
+            
+            }, {
+            'src': './img/proj4/5.jpg',
+            
+            }, {
+            'src': './img/proj4/6.jpg',
+            
+            }, {
+            'src': './img/proj4/7.jpg',
+            
+            }]
+    })
+ 
+});
+
+
+
+
+$('#gallery-icon-5,#gallery-link-5').on('click', function() {
+ 
+    $(this).lightGallery({
+        dynamic: true,
+        download: false,
+        counter: false,
+        dynamicEl: [{
+            'src': './img/proj5/1.jpg',
+            
+            }, {
+            'src': './img/proj5/2.jpg',
+            
+            }, {
+            'src': './img/proj5/3.jpg',
+            
+            }, {
+            'src': './img/proj5/4.jpg',
+            
+            }, {
+            'src': './img/proj5/5.jpg',
+            
+            }, {
+            'src': './img/proj5/6.jpg',
+            
+            }, {
+            'src': './img/proj5/7.jpg',
+            
+            }, {
+            'src': './img/proj5/8.jpg',
+            
+            }, {
+            'src': './img/proj5/9.jpg',
+            
+            }, {
+            'src': './img/proj5/10.jpg',
+            
+            }]
+    })
+ 
+});
+
+
+$('#gallery-icon-6,#gallery-link-6').on('click', function() {
+ 
+    $(this).lightGallery({
+        dynamic: true,
+        download: false,
+        counter: false,
+        dynamicEl: [{
+            'src': './img/proj6/1.jpg',
+            
+            }, {
+            'src': './img/proj6/2.jpg',
+            
+            }, {
+            'src': './img/proj6/3.jpg',
+            
+            }, {
+            'src': './img/proj6/4.jpg',
+            
+            }, {
+            'src': './img/proj6/5.jpg',
+            
+            }]
+    })
+ 
+});
+
+
+$('#gallery-icon-7,#gallery-link-7').on('click', function() {
+ 
+    $(this).lightGallery({
+        dynamic: true,
+        download: false,
+        counter: false,
+        dynamicEl: [{
+            'src': './img/proj7/1.jpg',
+            
+            }, {
+            'src': './img/proj7/2.jpg',
+            
+            }, {
+            'src': './img/proj7/3.jpg',
+            
+            }, {
+            'src': './img/proj7/4.jpg',
+            
+            }, {
+            'src': './img/proj7/5.jpg',
+            
+            }, {
+            'src': './img/proj7/6.jpg',
+            
+            }]
+    })
+ 
+});
+
+
+$('#gallery-icon-8,#gallery-link-8').on('click', function() {
+ 
+    $(this).lightGallery({
+        dynamic: true,
+        download: false,
+        counter: false,
+        dynamicEl: [{
+            'src': './img/proj8/1.jpg',
+            
+            }, {
+            'src': './img/proj8/2.jpg',
+            
+            }, {
+            'src': './img/proj8/3.jpg',
+            
+            }, {
+            'src': './img/proj8/4.jpg',
+            
+            }, {
+            'src': './img/proj8/5.jpg',
+            
+            }, {
+            'src': './img/proj8/6.jpg',
+            
+            }, {
+            'src': './img/proj8/7.jpg',
+            
+            }, {
+            'src': './img/proj8/8.jpg',
+            
+            }]
+    })
+ 
+});
+
+$('#gallery-icon-9,#gallery-link-9').on('click', function() {
+ 
+    $(this).lightGallery({
+        dynamic: true,
+        download: false,
+        counter: false,
+        dynamicEl: [{
+            'src': './img/proj9/1.jpg',
+            
+            }, {
+            'src': './img/proj9/2.jpg',
+            
+            }, {
+            'src': './img/proj9/3.jpg',
+            
+            }, {
+            'src': './img/proj9/4.jpg',
+            
+            }, {
+            'src': './img/proj9/5.jpg',
+            
+            }, {
+            'src': './img/proj9/6.jpg',
+            
+            }, {
+            'src': './img/proj9/7.jpg',
+            
+            }, {
+            'src': './img/proj9/8.jpg',
+            
+            }]
+    })
+ 
+});
 });
